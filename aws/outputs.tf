67a36a500268ff5fd524c2a8626268575c4f0106
kezhenxu94 <kezhenxu94@apache.org>
2022-12-13 11:21:40 +0800

    Add test results

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -9,3 +9,11 @@ output "database_address" {
 output "database_port" {
   value = var.db_type == "rds-postgresql" ? module.rds[0].db_instance_port : module.aurora[0].cluster_port
 }
+
+output "locust_address" {
+  value = "run command `open http://$(kubectl get svc -n locust -l app=locust-master -o jsonpath='{.items[0].status.loadBalancer.ingress[0].hostname}'):8089`"
+}
+
+output "skywalking_ui_address" {
+  value = "run command `kubectl -n istio-system port-forward $(kubectl -n istio-system get pod -l app=skywalking -l component=ui -o name) 8080:8080`"
+}
---
+++ outputs/postgresql/README.md
@@ -0,0 +1,2 @@
+test1: RPS~=6767
+test2: RPS~=26371
